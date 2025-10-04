d1b800d7170b3c01e1fe86f671e5bd946fcc643e
OscarGM-dot <andresmonge116@gmail.com>
2025-10-03 21:25:12 -0600

    Create Tarea2progra.py
    segunda tarea de progra de oscar

---
+++ Tarea2progra.py
@@ -0,0 +1,272 @@
+from tkinter import *
+root = Tk()
+
+cuadro = Frame()
+cuadro1 = Frame()
+cuadro2 = Frame()
+cuadro3 = Frame()
+cuadro4 = Frame()
+cuadro5 = Frame()
+cuadro6 = Frame()
+cuadro7 = Frame()
+cuadro8 = Frame()
+cuadro9 = Frame()
+cuadro10 = Frame()
+cuadro11 = Frame()
+cuadro12 = Frame()
+cuadro13 = Frame()
+cuadro14 = Frame()
+cuadro15 = Frame()
+cuadro16 = Frame()
+cuadro17 = Frame()
+cuadro18 = Frame()
+cuadro19 = Frame()
+cuadro20 = Frame()
+cuadro21 = Frame()
+cuadro22 = Frame()
+cuadro23 = Frame()
+cuadro24 = Frame()
+cuadro25 = Frame()
+cuadro26 = Frame()
+cuadro27 = Frame()
+cuadro28 = Frame()
+cuadro29 = Frame()
+cuadro30 = Frame()
+cuadro31 = Frame()
+cuadro32 = Frame()
+cuadro33 = Frame()
+cuadro34 = Frame()
+cuadro35 = Frame()
+cuadro36 = Frame()
+cuadro37 = Frame()
+cuadro38 = Frame()
+cuadro39 = Frame()
+cuadro40 = Frame()
+cuadro41 = Frame()
+cuadro42 = Frame()
+cuadro43 = Frame()
+cuadro44 = Frame()
+cuadro45 = Frame()
+cuadro46 = Frame()
+cuadro47 = Frame()
+cuadro48 = Frame()
+cuadro49 = Frame()
+cuadro50 = Frame()
+cuadro51 = Frame()
+cuadro52 = Frame()
+cuadro53 = Frame()
+cuadro54 = Frame()  
+cuadro55 = Frame()
+cuadro56 = Frame()
+cuadro57 = Frame()
+cuadro58 = Frame()
+cuadro59 = Frame()
+cuadro60 = Frame()
+cuadro61 = Frame()
+cuadro62 = Frame()
+cuadro63 = Frame()
+cuadro64 = Frame()
+cuadro65 = Frame()
+cuadro66 = Frame()
+cuadro67 = Frame()
+cuadro68 = Frame()
+cuadro69 = Frame()
+cuadro70 = Frame()
+cuadro71 = Frame()
+cuadro72 = Frame()
+cuadro73 = Frame()
+cuadro74 = Frame()
+cuadro75 = Frame()
+cuadro76 = Frame()
+cuadro77 = Frame()
+cuadro78 = Frame()
+cuadro79 = Frame()
+cuadro80 = Frame()
+
+
+cuadro.config(width=20, height=20, bg="yellow")
+cuadro.grid(row=1, column=1)
+
+cuadro1.config(width=20, height=20, bg="yellow")
+cuadro1.grid(row=2, column=1)
+
+cuadro2.config(width=20, height=20, bg="yellow")
+cuadro2.grid(row=3,column=1)
+
+cuadro3.config(width=20, height=20, bg="yellow")
+cuadro3.grid(row=4, column=1)
+
+cuadro4.config(width=20, height=20, bg="yellow")
+cuadro4.grid(row=5, column=1)
+
+cuadro5.config(width=20, height=20, bg="yellow")
+cuadro5.grid(row=6, column=1)
+
+cuadro6.config(width=20, height=20, bg="yellow")
+cuadro6.grid(row=1, column=2)
+
+cuadro7.config(width=20, height=20, bg="white")
+cuadro7.grid(row=2, column=2)
+
+cuadro8.config(width=20, height=20, bg="white")
+cuadro8.grid(row=3, column=2)
+
+cuadro9.config(width=20, height=20, bg="white")
+cuadro9.grid(row=4, column=2)
+
+cuadro10.config(width=20, height=20, bg="yellow")
+cuadro10.grid(row=5, column=2)
+
+cuadro11.config(width=20, height=20, bg="yellow")
+cuadro11.grid(row=6, column=2)
+
+cuadro12.config(width=20, height=20, bg="white")
+cuadro12.grid(row=7, column=1)
+
+cuadro13.config(width=20, height=20, bg="white")
+cuadro13.grid(row=7, column=2)
+
+cuadro14.config(width=20, height=20, bg="brown")
+cuadro14.grid(row=8, column=1)
+
+cuadro15.config(width=20, height=20, bg="brown")
+cuadro15.grid(row=8, column=2)
+
+cuadro16.config(width=20, height=20, bg="yellow")
+cuadro16.grid(row=1, column=3)
+
+cuadro17.config(width=20, height=20, bg="white")
+cuadro17.grid(row=2, column=3)
+
+cuadro18.config(width=20, height=20, bg="black")
+cuadro18.grid(row=3, column=3)
+
+cuadro19.config(width=20, height=20, bg="white")
+cuadro19.grid(row=4, column=3)
+
+cuadro20.config(width=20, height=20, bg="yellow")
+cuadro20.grid(row=5, column=3)
+
+cuadro21.config(width=20, height=20, bg="yellow")
+cuadro21.grid(row=6, column=3)
+
+cuadro22.config(width=20, height=20, bg="white")
+cuadro22.grid(row=7, column=3)
+
+cuadro23.config(width=20, height=20, bg="brown")
+cuadro23.grid(row=8, column=3)
+
+cuadro24.config(width=20, height=20, bg="black")
+cuadro24.grid(row=9, column=2)
+
+cuadro25.config(width=20, height=20, bg="black")
+cuadro25.grid(row=9, column=3)
+
+cuadro26.config(width=20, height=20, bg="yellow")
+cuadro26.grid(row=1, column=4)
+
+cuadro27.config(width=20, height=20, bg="yellow")
+cuadro27.grid(row=2, column=4)
+
+cuadro28.config(width=20, height=20, bg="yellow")
+cuadro28.grid(row=3, column=4)
+
+cuadro29.config(width=20, height=20, bg="yellow")
+cuadro29.grid(row=4, column=4)
+
+cuadro30.config(width=20, height=20, bg="yellow")
+cuadro30.grid(row=5, column=4)
+
+cuadro31.config(width=20, height=20, bg="yellow")
+cuadro31.grid(row=6, column=4)
+
+cuadro32.config(width=20, height=20, bg="red")
+cuadro32.grid(row=7, column=4)
+
+cuadro33.config(width=20, height=20, bg="red")
+cuadro33.grid(row=8, column=4)
+
+cuadro34.config(width=20, height=20, bg="yellow")
+cuadro34.grid(row=1, column=5)
+
+cuadro35.config(width=20, height=20, bg="white")
+cuadro35.grid(row=2, column=5)
+
+cuadro36.config(width=20, height=20, bg="black")
+cuadro36.grid(row=3, column=5)
+
+cuadro37.config(width=20, height=20, bg="white")
+cuadro37.grid(row=4, column=5)
+
+cuadro38.config(width=20, height=20, bg="yellow")
+cuadro38.grid(row=5, column=5)
+
+cuadro39.config(width=20, height=20, bg="yellow")
+cuadro39.grid(row=6, column=5)
+
+cuadro40.config(width=20, height=20, bg="white")
+cuadro40.grid(row=7, column=5)  
+
+cuadro41.config(width=20, height=20, bg="brown")
+cuadro41.grid(row=8, column=5)
+
+cuadro42.config(width=20, height=20, bg="black")
+cuadro42.grid(row=9, column=5)
+
+cuadro43.config(width=20, height=20, bg="yellow")
+cuadro43.grid(row=1, column=6)
+
+cuadro44.config(width=20, height=20, bg="white")
+cuadro44.grid(row=2, column=6)
+
+cuadro45.config(width=20, height=20, bg="white")
+cuadro45.grid(row=3, column=6)
+
+cuadro46.config(width=20, height=20, bg="white")
+cuadro46.grid(row=4, column=6)
+
+cuadro47.config(width=20, height=20, bg="yellow")
+cuadro47.grid(row=5, column=6)
+
+cuadro48.config(width=20, height=20, bg="yellow")
+cuadro48.grid(row=6, column=6)
+
+cuadro49.config(width=20, height=20, bg="white")
+cuadro49.grid(row=7, column=6)
+
+cuadro50.config(width=20, height=20, bg="brown")    
+cuadro50.grid(row=8, column=6)  
+
+cuadro51.config(width=20, height=20, bg="black")
+cuadro51.grid(row=9, column=6)
+
+cuadro52.config(width=20, height=20, bg="yellow")
+cuadro52.grid(row=1, column=7)
+
+cuadro53.config(width=20, height=20, bg="yellow")
+cuadro53.grid(row=2, column=7)
+
+cuadro54.config(width=20, height=20, bg="yellow")
+cuadro54.grid(row=3, column=7)  
+
+cuadro55.config(width=20, height=20, bg="yellow")
+cuadro55.grid(row=4, column=7)  
+
+cuadro56.config(width=20, height=20, bg="yellow")
+cuadro56.grid(row=5, column=7)
+
+cuadro57.config(width=20, height=20, bg="yellow")
+cuadro57.grid(row=6, column=7)
+
+cuadro58.config(width=20, height=20, bg="white")
+cuadro58.grid(row=7, column=7)
+
+cuadro59.config(width=20, height=20, bg="brown")
+cuadro59.grid(row=8, column=7)
+
+
+
+
+
+
+root.mainloop()
